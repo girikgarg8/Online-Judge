62d3660992a47b6793cbd20d0ac0790d710364c0
Girik Garg <girikgarg8@gmail.com>
2024-09-22 17:44:45 +0530

    feat: add not implemented error for api contract

--- Problem-Service/src/controllers/problem.controller.js
+++ Problem-Service/src/controllers/problem.controller.js
@@ -1,12 +1,34 @@
-function addProblem(req, res) {}
+const { StatusCodes } = require("http-status-codes");
 
-function getProblem(req, res) {}
+function addProblem(req, res) {
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not implemented" });
+}
 
-function getProblems(req, res) {}
+function getProblem(req, res) {
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not implemented" });
+}
 
-function deleteProblem(req, res) {}
+function getProblems(req, res) {
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not implemented" });
+}
 
-function updateProblem(req, res) {}
+function deleteProblem(req, res) {
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not implemented" });
+}
+
+function updateProblem(req, res) {
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not implemented" });
+}
 
 module.exports = {
   addProblem,
