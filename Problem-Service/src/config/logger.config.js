88f30c0b90560f092af42fd315f455c72d9ace73
Girik Garg <girikgarg8@gmail.com>
2024-09-29 04:42:42 +0530

    feat: integrate cosmos db for storing logs

--- Problem-Service/src/config/logger.config.js
+++ Problem-Service/src/config/logger.config.js
@@ -2,8 +2,27 @@ const winston = require("winston");
 const { combine, timestamp, printf, colorize, align } = winston.format;
 require("winston-mongodb");
 const { LOGS_ATLAS_DB_URL } = require("./server.config");
+const { Writable } = require("stream");
+const { logToCosmosDB } = require("../clientapis/cosmosClient");
+const { extractDetailsFromLog } = require("../utils");
+
+const customStream = new Writable({
+  write(chunk, encoding, callback) {
+    const chunkMessage = chunk.toString();
+    const { message, level } = extractDetailsFromLog(chunkMessage);
+    logToCosmosDB(level, message);
+    callback();
+  },
+});
+
+const customStreamTransport = new winston.transports.Stream({
+  level: "error",
+  stream: customStream,
+});
 
 const allowedTransports = [];
+
+allowedTransports.push(customStreamTransport);
 
 // The below transport configuration enables logging in console
 allowedTransports.push(
